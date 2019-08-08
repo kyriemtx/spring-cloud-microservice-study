8483cd2f924abb6146f81a4794f05abff24fc9d5
tengxiao.ma <tengxiao.ma@huifu.com>
2019-08-08 18:34:00 +0800

    在SpringCloud中使用Netflix开发的Hystrix实现段容器

---
+++ microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/kyrie/hystrix/controller/MovieRibbonHystrixController.java
@@ -0,0 +1,25 @@
+package com.kyrie.hystrix.controller;
+
+import com.kyrie.hystrix.entity.User;
+import com.kyrie.hystrix.service.RibbonHystrixService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MovieRibbonHystrixController
+ * @Description
+ * @Author Kyrie
+ * @Date 2019/8/8 18:28
+ * @Version 1.0
+ **/
+@RestController
+public class MovieRibbonHystrixController {
+    @Autowired
+    private RibbonHystrixService ribbonHystrixService;
+
+    @GetMapping("/one")
+    public User selectByPrimaryKey(Long id){
+        return ribbonHystrixService.selectByPrimaryKey(id);
+    }
+}
---
+++ microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/kyrie/hystrix/service/RibbonHystrixService.java
@@ -0,0 +1,42 @@
+package com.kyrie.hystrix.service;
+
+import com.kyrie.hystrix.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
+
+/**
+ * @ClassName RibbonHystrixService
+ * @Description
+ * @Author Kyrie
+ * @Date 2019/8/8 18:14
+ * @Version 1.0
+ **/
+@Service
+public class RibbonHystrixService {
+    @Autowired
+    private RestTemplate restTemplate;
+   // private static final Logger LOGGER = LoggerFactory.getLogger(RibbonHystrixService.class);
+    /**
+     * 调用服务提供者提供的服务查询用户信息
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "fallback")
+    public User selectByPrimaryKey(Long id){
+
+        User user = restTemplate.getForObject("http://MICROSERVICE-PROVIDER-USER/one/"+id,User.class);
+        return user;
+
+    }
+    public User fallback(Long id){
+        User user = new User();
+        user.setAge(88);
+        user.setId(5L);
+        user.setUsername("kkkkkkkk");
+        return  user;
+    }
+}
